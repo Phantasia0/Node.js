b3e63f4a0c4a5910bb85f6be9a9820fa8ec6a5ed
Phantasia0 <ncprog1@gmail.com>
2022-09-02 07:10:59 +0900

    chap 7 go

---
+++ ch4/cookie2.js
@@ -0,0 +1,5 @@
+const http= require('http');
+const fs = require('fs').promises;
+const url = require('url');
+const qs = require('querystring');
+
---
+++ ch6/6.5.1/learn-express/routes/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/',(req,res)=>{
+    res.send('Hello, Express');
+});
+
+module.exports = router;
+
+router.get('/',function(req,res,next){
+    //res.render('index',{title: 'Express'});
+    res.locals.title = 'Express';
+    res.render('index');
+});
